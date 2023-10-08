7826f0d7df2bc92653feb27a3af8e2a93db02122
Yusuf Vohra <121709213+Dora1emon76@users.noreply.github.com>
2023-10-09 00:19:15 +0530

    Update server.py

--- server.py
+++ server.py
@@ -9,7 +9,7 @@ import atexit
 import os
 
 # List of patterns to match
-patterns = ['#Ad', '#branԁDiscount', '#paidAD', '#paidad', '#AD', '#Paidad', '#PaidAD', 'bots.business/ads', '#PaidAd','#PromotіonInғ1uencer','#sales','#influеncermarketіпg','#placementAd', 'sponsored','#AdvertisementMarketing']
+patterns = ['#','#Ad', '#branԁDiscount', '#paidAD', '#paidad', '#AD', '#Paidad', '#PaidAD', 'bots.business/ads', '#PaidAd','#PromotіonInғ1uencer','#sales','#influеncermarketіпg','#placementAd', 'sponsored','#AdvertisementMarketing']
 
 # Combine the patterns into a single regex pattern
 pattern = '|'.join(re.escape(p) for p in patterns)
@@ -25,11 +25,11 @@ def send_post_request(bot_token, data):
 
 # Start serveo and create a tunnel
 def start_serveo():
-    serveo_process = subprocess.Popen(['ssh', '-R', '80:localhost:88', 'serveo.net'])
+    serveo_process = subprocess.Popen(['ssh', '-R', '80:localhost:88', 'serveo.net'], stdout=subprocess.PIPE)
     atexit.register(lambda: serveo_process.terminate())
-    os.environ["SERVEO_URL"] = 'https://'+serveo_process.args[-1]+'.serveo.net'
+    serveo_url = serveo_process.communicate()[0].decode().strip()
+    os.environ["SERVEO_URL"] = serveo_url
     print(f'serveo URL: {os.environ["SERVEO_URL"]}')
-
 # serveo initialization
 serveo_thread = threading.Thread(target=start_serveo)
 serveo_thread.start()
@@ -39,7 +39,7 @@ def set_webhook():
     bot_token = request.args.get("token")
     # Use serveo public URL as the webhook URL
     serveo_url = os.environ.get("SERVEO_URL", "https://localhost:88")
-    requests.get(f"https://api.telegram.org/bot{bot_token}/setWebhook?url=https://sperno.serveo.net/tg_webhook?token={bot_token}")
+    requests.get(f"https://api.telegram.org/bot{bot_token}/setWebhook?url=https://{serveo_url}/tg_webhook?token={bot_token}")
     return "SUCCESS!"
 
 @app.route("/")
