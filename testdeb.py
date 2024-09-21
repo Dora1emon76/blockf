374d43fe21d3493bc1bde596d955473d673c5854
Yusuf Vohra <121709213+Dora1emon76@users.noreply.github.com>
2024-09-21 14:04:43 +0530

    Add files via upload

---
+++ testdeb.py
@@ -0,0 +1,85 @@
+import asyncio
+import requests
+from flask import Flask, request
+import pyrogram
+import threading
+import re
+from pyngrok import ngrok  # Install pyngrok using pip install pyngrok
+
+# List of patterns to match
+patterns = ['#', '#Ad', '#branԁDiscount', '#paidAD', '#paidad', '#AD', '#Paidad', '#PaidAD', 'bots.business/ads', '#PaidAd', '#PromotіonInғ1uencer', '#sales', '#influеncermarketіпg', '#placementAd', 'sponsored', '#AdvertisementMarketing']
+
+# Combine the patterns into a single regex pattern
+pattern = '|'.join(re.escape(p) for p in patterns)
+
+app = Flask(__name__)
+
+# Function to send the post request in a separate thread
+def send_post_request(bot_token, data):
+    try:
+        requests.post("https://api.bots.business/tg_webhooks/" + bot_token, json=data)
+    except Exception as e:
+        print(e)
+
+# Start ngrok and create a tunnel
+#public_url = ngrok.connect(port=5000)
+
+#print(" * ngrok tunnel \"{}\" -> \"http://127.0.0.1:{}/\"".format(public_url, 5000))
+
+#def get_ngrok_url():
+   # return public_url
+
+@app.route("/set")
+def set_webhook():
+    bot_token = request.args.get("token")
+    # Use ngrok public URL as the webhook URL
+   # ngrok_url = get_ngrok_url()
+    requests.get(f"https://api.telegram.org/bot{bot_token}/setWebhook?url=https://bbadblock.vercel.app/tg_webhook?token={bot_token}")
+    return "SUCCESS!"
+
+@app.route("/")
+def hello():
+    return "Hello, World!"
+
+@app.route("/tg_webhook", methods=["GET", "POST"])
+def handle_webhook():
+    try:
+        data = request.get_json()
+        bot_token = request.args.get("token")
+
+        # Start a separate thread to send the post request
+        post_thread = threading.Thread(target=send_post_request, args=(bot_token, data))
+        post_thread.start()
+
+        async def run_and_idle(bot):
+            async with bot:
+                current_msg_id = data['message']['message_id']
+                chat_id = data['message']['chat']['id']
+                for i in range(1, 10):
+                    message = await bot.get_messages(chat_id, current_msg_id + i)
+
+                    # Check if the message text matches any pattern
+                    if re.search(pattern, message.text):
+                        await message.delete()
+                        print(f'Message with id {current_msg_id + i} deleted successfully')
+
+        # Define the loop
+        loop = asyncio.new_event_loop()
+        asyncio.set_event_loop(loop)
+
+        # bot = pyrogram.Client(name="mylient" + bot_token, bot_token=bot_token, api_id=28905453, api_hash="21073abfe2bf0652881caf8ec8f104e4")
+        bot = pyrogram.Client(
+            name=":memory:" + bot_token,
+            bot_token=bot_token,
+            api_id=28905453,
+            api_hash="21073abfe2bf0652881caf8ec8f104e4",
+            in_memory=True,
+        )
+        loop.run_until_complete(run_and_idle(bot))
+        return "got it", 200
+    except Exception as e:
+        print(f"error: {e}")
+        return "Hi", 200
+
+if __name__ == '__main__':
+    app.run(port=80 )
